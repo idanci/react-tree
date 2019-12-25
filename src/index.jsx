641481d0741121f7d0a1f2d193f7780d1ac18233
Ivan Danci <bah4uk@gmail.com>
2019-12-25 16:31:08 +0200

    Remove unused code

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <App title='React tree example' />,
+  document.getElementById('root'),
+);
